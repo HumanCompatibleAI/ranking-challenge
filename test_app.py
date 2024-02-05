3e8652f0665dcc0c1687090f65d9af3d4f29f502
Sana Pandey <sanapandey@berkeley.edu>
2024-02-05 11:55:55 -0800

    sample code working locally

--- test_app.py
+++ test_app.py
@@ -2,17 +2,46 @@ import requests
 import time
 
 # Sample data containing multiple text items
-data = [
-    {"text": "I love this product! It's amazing."},
-    {"text": "This movie was terrible. I wouldn't recommend it."},
-    {"text": "The food at that restaurant was average."},
-]
+data = {
+    "session": {
+        "user_id": "193a9e01-8849-4e1f-a42a-a859fa7f2ad3",
+        "user_name_hash": "6511c5688bbb87798128695a283411a26da532df06e6e931a53416e379ddda0e",
+        "current_time": "2024-01-20 18:41:20",
+    },
+    "items": [
+        {
+            "id": "de83fc78-d648-444e-b20d-853bf05e4f0e",
+            "title": "this is the post title, available only on reddit",
+            "text": "this is a social media post",
+            "author_name_hash": "60b46b7370f80735a06b7aa8c4eb6bd588440816b086d5ef7355cf202a118305",
+            "type": "post",
+            "platform": "reddit",
+            "created_at": "2023-12-06 17:02:11",
+            "enagements": {
+                "upvote": 34,
+                "downvote": 27
+            }
+        },
+        {
+            "id": "a4c08177-8db2-4507-acc1-1298220be98d",
+            "text": "this is a comment, by the author of the post",
+            "author_name_hash": "60b46b7370f80735a06b7aa8c4eb6bd588440816b086d5ef7355cf202a118305",
+            "type": "comment",
+            "platform": "reddit",
+            "created_at": "2023-12-08 11:32:12",
+            "enagements": {
+                "upvote": 3,
+                "downvote": 5
+            }
+        }
+    ]
+}
 
 # Wait for the Flask app to start up
 time.sleep(2)
 
 # Send POST request to the API
-response = requests.post("http://localhost:5000/analyze_sentiment", json=data)
+response = requests.post("http://localhost:5001/analyze_sentiment", json=data)
 
 # Check if the request was successful (status code 200)
 if response.status_code == 200:
